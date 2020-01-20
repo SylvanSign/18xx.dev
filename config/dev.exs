767f59c000d74401d6599d9386e44c3c452e4774
Jake Bray <jake.d.bray@gmail.com>
2020-01-20 12:26:56 -0500

    fix live_reload pattern for ex files

--- config/dev.exs
+++ config/dev.exs
@@ -51,7 +51,7 @@ config :trains, TrainsWeb.Endpoint,
     patterns: [
       ~r"priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$",
       ~r"priv/gettext/.*(po)$",
-      ~r"lib/trains_web/{live,views}/.*(ex)$",
+      ~r"lib/trains_web/(live,views)/.*(ex)$",
       ~r"lib/trains_web/templates/.*(eex)$"
     ]
   ]
