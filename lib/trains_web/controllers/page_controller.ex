12741c6248f8fc84dc8c53b1eeb704c2bac54a40
Jake Bray <jake.d.bray@gmail.com>
2020-01-22 20:32:14 -0500

    wip testing

--- lib/trains_web/controllers/page_controller.ex
+++ lib/trains_web/controllers/page_controller.ex
@@ -1,7 +1,14 @@
 defmodule TrainsWeb.PageController do
   use TrainsWeb, :controller
 
+  @dimension 500
+
   def index(conn, _params) do
-    render(conn, "index.html")
+    render(conn, "index.html",
+      game: "1889",
+      height: @dimension,
+      width: @dimension,
+      viewBox: "-87.6025 -76 175.205 152"
+    )
   end
 end
